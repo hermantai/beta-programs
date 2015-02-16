6e51942d6af6b99530b0c34d8e43985853e849ef
Herman Tai <htaihm@gmail.com>
2015-02-16 13:05:19 -0800

    [myurl] Add bootstrap styling and use jquery instead of native dom javascript api

--- beta-programs/myurl/js/myurl.js
+++ beta-programs/myurl/js/myurl.js
@@ -1,3 +1,16 @@
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) {
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+
 function ends_with(str, suffix) {
   return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
@@ -39,68 +52,57 @@ function setup_components() {
   ];
 
   var hide_admin_urls = ends_with(window.location.href, "?reveal_all=true");
-  var container = document.getElementById('my_urls_container');
+  var container = $('#my-urls-container');
 
   for (var i = 0; i < smart_urls.length; i++) {
     var smart_url = smart_urls[i];
-    var f = document.createElement('form');
     if (smart_url['is_hidden'] && !hide_admin_urls) {
       console.log('Not revealing "' + smart_url['name'] + '"');
       continue;
     }
-    f.setAttribute('id', smart_url['name']);
 
-    var input_ele = document.createElement('input');
-    var input_ele_id = smart_url['name'] + "_input";
-    input_ele.setAttribute('type', 'text');
-    input_ele.setAttribute('name', input_ele_id);
-    input_ele.setAttribute('id', input_ele_id);
-    input_ele.setAttribute('size', 20);
+    var form = $(
+      '<form id="{0}" role="form" class="form-inline"></form>'.format(
+        smart_url['name']
+      )
+    );
 
-    f.appendChild(input_ele);
+    var form_group = $('<div class="form-group"></div>');
+    form.append(form_group);
 
-    var submit_ele = document.createElement('input');
-    submit_ele.setAttribute('type', 'submit');
-    submit_ele.setAttribute('value', smart_url['name']);
+    var input_element_id = smart_url['name'] + "_input";
+    var input_element = $(
+      '<input type="text" name="{0}" id="{0}" class="form-control" />'.format(
+        input_element_id
+      )
+    );
 
-    submit_ele.onclick = (function(my_smart_url, my_input_ele) {
+    form_group.append(input_element);
+
+    var redirect_func = (function(my_smart_url, my_input_element) {
       return function (e) {
-        var input_value = encodeURIComponent(my_input_ele.value);
+        e.preventDefault();
+        var input_value = encodeURIComponent(my_input_element.val());
         var url = my_smart_url['url'].replace('%s', input_value);
         console.log("Going to " + url);
-        window.location.href = url;
-        return false;
+        window.open(url, "_blank");
+        my_input_element.focus();
       };
-    })(smart_url, input_ele);
+    })(smart_url, input_element);
 
-    f.appendChild(submit_ele);
+    var submit_element = $(
+      '<button type="submit" class="btn btn-default">{0}</button>'.format(smart_url['name'])
+    );
+    submit_element.click(redirect_func);
+    form_group.append(submit_element);
 
-    var link_ele = document.createElement('a');
-    link_ele.href = "#";
+    var redirect_link = $(
+      '<a href="#">{0}</a>'.format(smart_url['name'])
+    );
+    redirect_link.click(redirect_func);
+    form_group.append(redirect_link);
 
-    var link_text_node = document.createTextNode(smart_url['name']);
-    link_ele.appendChild(link_text_node);
-
-    link_ele.onclick = (function(my_link_ele, my_smart_url, my_input_ele) {
-      return function (e) {
-        // set the href of the anchor
-        var input_value = encodeURIComponent(my_input_ele.value);
-        var url = my_smart_url['url'].replace('%s', input_value);
-        console.log("Going to " + url);
-        my_link_ele.href = url;
-
-        // clear the input value, so we can easily type another one and
-        // click the link again
-        my_input_ele.value = '';
-        my_input_ele.focus();
-        return true;
-      };
-    })(link_ele, smart_url, input_ele);
-
-
-    f.appendChild(link_ele);
-
-    container.appendChild(f);
+    container.append(form);
   }
 }
 
