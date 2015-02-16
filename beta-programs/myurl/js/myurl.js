ecb0043cdce7d60ee7bf545a1c60217694018b9b
Herman Tai <htaihm@gmail.com>
2015-02-16 15:26:42 -0800

    [myurl] Add "Settings" tab, "Sync up all text inputs" setting
    Summary:
    Add "Settings" tab, and add a new "Sync up all text inputs" setting to it.
    
    If "Sync up all text inputs" is checked, all text inputs are in sync as the
    user types on any one of them.

--- beta-programs/myurl/js/myurl.js
+++ beta-programs/myurl/js/myurl.js
@@ -11,12 +11,59 @@ if (!String.prototype.format) {
 }
 
 
+var myurl = {
+  settings: {
+    _fields: {
+      "is_sync_text_inputs": {
+        type: "boolean"
+      }
+    },
+
+    init: function () {
+      this.load_settings();
+      this.log_settings();
+
+      $("#sync-text-inputs-checkbox").prop("checked", this.is_sync_text_inputs);
+    },
+
+    load_settings: function () {
+      for (var field_name in this._fields) {
+        var field = this._fields[field_name];
+        var value = localStorage.getItem(field_name);
+        if (field.type === "boolean") {
+          value = value && value === "true" ? true : false;
+        }
+        this[field_name] = value;
+      }
+    },
+
+    log_settings: function () {
+      console.log("Settings:");
+      for (var field_name in this._fields) {
+        console.log("{0} -> {1}".format(field_name, this[field_name]));
+      }
+    },
+
+    set: function (field_name, value) {
+      if (!this.hasOwnProperty(field_name)) {
+        console.error("No field [{0}] in settings".format(field_name));
+      }
+
+      localStorage.setItem(field_name, value);
+      this[field_name] = value;
+    }
+  }
+};
+
+
 function ends_with(str, suffix) {
   return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
 
 function setup_components() {
+  // Set up the "Home" page
+
   var smart_urls = [
     {
       'name': 'Finance Yahoo',
@@ -54,6 +101,7 @@ function setup_components() {
 
   var hide_admin_urls = ends_with(window.location.href, "?reveal_all=true");
   var container = $('#my-urls-container');
+  var text_input_elements = [];
 
   for (var i = 0; i < smart_urls.length; i++) {
     var smart_url = smart_urls[i];
@@ -77,7 +125,22 @@ function setup_components() {
         input_element_id
       )
     );
+    input_element.keyup(
+      function () {
+        if (myurl.settings.is_sync_text_inputs) {
+          // Iterate through all text input elements
+          // Note that text_input_elements is defined at the beginning of
+          // setup_components function, so it containx all text input elements
+          // when the following code runs.
+          var text_value = this.value;
+          for (var j = 0; j < text_input_elements.length; j++) {
+            text_input_elements[j].val(text_value);
+          }
+        }
+      }
+    );
 
+    text_input_elements.push(input_element);
     form_group.append(input_element);
 
     var redirect_func = (function(my_smart_url, my_input_element) {
@@ -105,6 +168,15 @@ function setup_components() {
 
     container.append(form);
   }
+
+  // Set up the "Settings" page
+  myurl.settings.init();
+
+  $("#sync-text-inputs-checkbox").change(
+    function () {
+      myurl.settings.set('is_sync_text_inputs', this.checked);
+    }
+  );
 }
 
 
